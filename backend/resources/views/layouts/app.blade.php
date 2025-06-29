b989e91d6f715c8e298ddb00c8546fb0a2e35bdd
khiem377 <khiemhgps39587@gmail.com>
2025-06-29 09:21:12 +0700

    fix

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -157,9 +157,8 @@
 
     <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script>
-        const API_BASE_URL = '{{ env('APP_URL') }}/api';
-
+   <script>
+    const API_BASE_URL = '{{ config('app.url') }}/api';
         function logout() {
             const token = localStorage.getItem('access_token');
             fetch(`${API_BASE_URL}/logout`, {
